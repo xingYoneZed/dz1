d34e2117f4cae7d222ff0480a1a8a01e5dc1efb6
xingYoneZed <darklordchinchin121212@yandex.ru>
2022-11-15 18:02:53 +0300

    1

--- game life/game life.cpp
+++ game life/game life.cpp
@@ -1,59 +1,84 @@
-﻿
-#include <fstream>
-#include <iostream>
-
-int main() {
-    std::fstream kek;
-    kek.open("in.txt");
-
-    if(!in.is_open())
-    {
-        std::cout >> "minus";
-    }
+﻿#include <iostream>             // Header Files
+#include  <conio.h>
+#include   <cstdlib>
+#include   <windows.h>
+#include  <time.h>
 
 
-    kek.seekg(0);
-
-    int rows = 0, cols = 0;
+using namespace std;
 
 
-    kek >> cols;
-    kek >> rows;
-
-    kek.seekg(3);
-
-
-    int** arr;
-    arr = new int* [cols];
-    for (int i = 0; i < cols; i++) arr[i] = new int[rows];
+void gotoxy(int x, int y)
+{
+    COORD coordinates;    
+    coordinates.X = x;     
+    coordinates.Y = y;     
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coordinates);
+}
 
 
-    for (int i = 0; i < cols; i++) {
-        for (int j = 0; j < rows; j++) {
-            kek >> arr[i][j];
+int main()
+{
+    int i, j, data[83][60], nextarray[83][60];
+    int around;
+    srand(time(NULL));
+
+    for (i = 0; i <= 79; i++)
+        for (j = 0; j <= 56; j++)
+        {
+            data[i][j] = 0;
+
+            if (i > 1 && i < 78 && j>1 && j < 55)
+                if (rand() % 10 == 1)
+                {
+                    data[i][j] = 1;
+                }
         }
-    }
 
-
-
-
-
-    for (int i = 0; i < cols; i++) {
-        for (int j = rows - 1; j >= 0; j--) {
-            std::cout << " " << arr[i][j];
-
-        }
-        std::cout << std::endl;
-    }
-
-
-
-
-    for (int i = 0; i < cols; i++)
+    while (1)
     {
-        delete[] arr[i];
+
+        for (i = 2; i <= 77; i++)
+            for (j = 2; j <= 54; j++)
+            {
+                gotoxy(i, j);
+                if (data[i][j]) cout << "\2"; else cout << " ";
+
+            }
+
+
+        for (i = 2; i <= 77; i++)
+            for (j = 2; j <= 54; j++)
+            {
+
+                nextarray[i][j] = data[i][j];
+                around = data[i - 1][j - 1] + data[i][j - 1] + data[i + 1][j - 1] + data[i - 1][j] + data[i + 1][j] + data[i - 1][j + 1] + data[i][j + 1] + data[i + 1][j + 1];
+                if (data[i][j] == 1)
+                {
+                    if (around < 2 || around>3) nextarray[i][j] = 0; else nextarray[i][j] = 1;
+
+
+                }
+                else
+                {
+                    if (around == 3) nextarray[i][j] = 1;
+                }
+
+            }
+
+        for (i = 2; i <= 77; i++)
+            for (j = 2; j <= 54; j++)
+                data[i][j] = nextarray[i][j];
+
+        Sleep(100);
+
+
     }
-    delete[] arr;
 
 
+
+
+
+
+    return 0;
 }
